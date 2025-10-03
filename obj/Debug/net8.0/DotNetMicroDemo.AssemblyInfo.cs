b7619eb612b2899ddaed86b10c38555efab40b2e
Christo <gclotriet@outlook.com>
2025-10-02 20:04:53 -0400

    Fix GitHub language stats and clean up portfolio text
    - Add .gitattributes to properly classify C# and exclude utility files
    - Mark Python QR generator as vendored (excluded from language stats)
    - Remove 'controller code' reference from portfolio description
    - This should help C# show up properly in GitHub language statistics

--- obj/Debug/net8.0/DotNetMicroDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetMicroDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetMicroDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30b3fd624f1b1a1aa70dd30c0b2127119921baf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80197aeb828dc804fe5e910ff9420dd063002cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetMicroDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetMicroDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
